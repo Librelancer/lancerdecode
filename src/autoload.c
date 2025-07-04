cbb34fba33d500d5b739f74a0db2b6fd80cc7763
CallumDev <callum.mcging@gmail.com>
2025-07-05 02:40:45 +0930

    Fix MP3 detection, segfault on error null

--- src/autoload.c
+++ src/autoload.c
@@ -62,8 +62,28 @@ ld_pcmstream_t ogg_getstream(ld_stream_t stream, ld_options_t options, const cha
     return NULL;
 }
 
+static int drmp3_hdr_valid(unsigned char *h)
+{
+    #define DRMP3_HDR_GET_LAYER(h)            (((h[1]) >> 1) & 3)
+    #define DRMP3_HDR_GET_BITRATE(h)          ((h[2]) >> 4)
+    #define DRMP3_HDR_GET_SAMPLE_RATE(h)      (((h[2]) >> 2) & 3)
+    return h[0] == 0xff &&
+        ((h[1] & 0xF0) == 0xf0 || (h[1] & 0xFE) == 0xe2) &&
+        (DRMP3_HDR_GET_LAYER(h) != 0) &&
+        (DRMP3_HDR_GET_BITRATE(h) != 15) &&
+        (DRMP3_HDR_GET_SAMPLE_RATE(h) != 3);
+    
+    #undef DRMP3_HDR_GET_LAYER
+    #undef DRMP3_HDR_GET_BITRATE
+    #undef DRMP3_HDR_GET_SAMPLE_RATE
+}
+
 LDEXPORT ld_pcmstream_t ld_pcmstream_open(ld_stream_t stream, ld_options_t options, const char **error)
 {
+    // Provide valid error string pointer
+    const char *errorStack = NULL;
+    const char **errorOut = error ? error : &errorStack;
+
 	unsigned char magic[4];
 	/* Read in magic */
 	stream->read(magic,4,stream);
@@ -71,25 +91,22 @@ LDEXPORT ld_pcmstream_t ld_pcmstream_open(ld_stream_t stream, ld_options_t optio
 	/* Detect file type */
 	//Riff
 	if(memcmp(magic, "RIFF", 4) == 0) {
-		return riff_getstream(stream, options, error);
+		return riff_getstream(stream, options, errorOut);
 	}
 	//Ogg
 	if(memcmp(magic, "OggS", 4) == 0) {
-		return ogg_getstream(stream, options, error);
+		return ogg_getstream(stream, options, errorOut);
 	}
 	//Flac
 	if(memcmp(magic, "fLaC", 4) == 0) {
-		return flac_getstream(stream, options, error, 0);
+		return flac_getstream(stream, options, errorOut, 0);
 	}
 	//Mp3
-	if(memcmp(magic,"ID3", 3) == 0) {
-		return mp3_getstream(stream, options, error, -1,-1,-1,-1);
-	}
-	if(magic[0] == 0xFF && magic[1] == 0xFB) {
-		return mp3_getstream(stream, options, error, -1,-1,-1,-1);
+	if(memcmp(magic,"ID3", 3) == 0 || drmp3_hdr_valid(magic)) {
+		return mp3_getstream(stream, options, errorOut, -1,-1,-1,-1);
 	}
 
-    *error = "Unable to detect file type";
+    *errorOut = "Unable to detect file type";
 	LOG_O_ERROR(options, "Unable to detect file type");
 	return NULL;
 }
